91cfbdeadf7a6b03a658da2e418aa37e5c212d87
José L. Cañadas-Reche <canadasreche@gmail.com>
2020-03-15 15:11:27 +0100

    Revert "Evolutivo Países"

--- load_data.R
+++ load_data.R
@@ -5,8 +5,6 @@ library(tidyverse)
 library(leaflet)
 library(shinydashboard)
 
-source('model/generate_data.R')
-
 # nos bajamos  los datos, actualizados a día de ayer ----
 
 url <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
@@ -37,7 +35,27 @@ cvirus_longer <-  cvirus_longer %>%
 # mapa con el último dato -----
 ## sub graphs 
 
-res <- GenerateCountryEvolutionData(cvirus_longer)
+res  <- cvirus_longer %>% 
+    # filter(provincia_estado!= "Diamond Princess") %>% 
+    group_by(pais, fecha) %>% 
+    arrange(fecha) %>%
+    arrange(-casos) %>% 
+    summarise(casos = sum(casos),
+              Lat = first(Lat),
+              Long = first(Long)) %>% 
+    mutate(casos_prev_day = lag(casos, n = 1,  default = 0),
+           casos_nuevos = casos - casos_prev_day) %>% 
+    filter(casos >= 5) %>%
+    mutate(
+        casos_nuevos = if_else(casos_nuevos==0,
+                               lag(casos_nuevos,1),
+                               casos_nuevos),
+        dia_since_5 = row_number()) 
+
+
+
+
+
 
 
 cvirus_map_data <- res %>% 
