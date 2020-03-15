c49432d14a85e68e5932a94e6292c0660f3f6dd9
calejero <jcalejero@gmail.com>
2020-03-15 23:33:39 +0100

    Evolutivo dinámico

--- server_dir/tabla_evolutivo_server.R
+++ server_dir/tabla_evolutivo_server.R
@@ -4,31 +4,13 @@ library(plotly)
 
 output$tabla_evolutivo_paises <- renderPlotly({
   data.df <- GenerateCountryEvolutionData(cvirus_longer)
-  max.c <- max(data.df$fecha)
-  aux.df <- data.df[data.df$fecha == max.c, ]
-  aux.df <- aux.df[order(aux.df$casos, decreasing = TRUE), ]
-  aux.df <- aux.df[1:10, ]
-  data.df <- data.df[data.df$pais %in% aux.df$pais, ]
-  #data.df <- data.df[data.df$pais == 'Spain', ]
-  fig <- plot_ly(data.df, x = ~dia_since_5, y = ~casos, name = ~pais, type = 'scatter', mode = 'lines', color = ~pais
-                 ) 
-  
-  f <- list(
-    family = "Courier New, monospace",
-    size = 18,
-    color = "#7f7f7f"
-  )
-  
-  x <- list(
-    title = "Día desde alcanzar 5 casos",
-    titlefont = f
-  )
-  
-  y <- list(
-    title = "Número de casos",
-    titlefont = f
-  )
-  
-  fig <- fig %>% layout(xaxis = x, yaxis = y)
+  fig <- GenerateSummaryCountriesPlot(data.df, limit.c = input$limit, scale.c = input$scale, chart.c = 1)
+  fig
+})
+
+
+output$tabla_evolutivo_paises_crecimiento <- renderPlotly({
+  data.df <- GenerateCountryEvolutionData(cvirus_longer)
+  fig <- GenerateSummaryCountriesPlot(data.df, limit.c = input$limit, scale.c = input$scale, chart.c = 2)
   fig
 })
